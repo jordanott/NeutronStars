4716297707b00ddd84faa43eb501fdf8c5690368
Jordan Ott <jott1@uci.edu>
2021-03-17 17:37:24 -0700

    Poisson uncertainty estimation

--- main.py
+++ main.py
@@ -3,27 +3,18 @@
 """
 
 import os
-import sherpa
+import tqdm
 import pprint
+import numpy as np
 import tensorflow as tf
 import neutron_stars as ns
 
 
 # GET COMMAND LINE ARGS
 args = ns.parse_args()
-ns.utils.gpu_settings(args)
-
-# IF CONDUCTING HP SEARCH: GET ARGS
-if args['sherpa']:
-    client = sherpa.Client()
-    trial = client.get_trial()
-    args.update(trial.parameters)
-    args['trial_id'] = trial.id
-    args['sherpa_info'] = (client, trial)
 
 # SET UP THE DIRECTORY TO STORE RESULTS
 ns.utils.dir_set_up(args)
-ns.utils.store_settings(args)
 
 # SHOW ALL ARG SETTINGS
 pp = pprint.PrettyPrinter(indent=4)
@@ -33,6 +24,8 @@ pp.pprint(args)
 ns.paradigm_settings(args)
 
 if args['run_type'] == 'train':
+    ns.utils.store_settings(args)
+
     for fold in range(1, args['num_folds']+1):
         args['fold'] = fold
         
@@ -71,3 +64,45 @@ if args['run_type'] == 'train':
         ns.utils.predict_scale_store(data_loader.validation_gen, model, args, 'validation')
         if args['sherpa']:
             ns.utils.predict_scale_store(data_loader.test_gen, model, args, 'test')
+
+elif args['run_type'] == 'uncertain':
+    args['fold'] = 10
+    
+    # BUILD THE DATA LOADER & PARTITION THE DATASET
+    data_loader = ns.DataLoader(args)
+    # LOAD MODEL FROM CROSS-VALIDATION
+    model = tf.keras.models.load_model(args['model_dir'])
+    
+    X, Y = data_loader.validation_gen.load_all(transform=False)
+    
+    targets = np.zeros((100*Y.shape[0], Y.shape[1]))
+    predictions = np.zeros((100*Y.shape[0], Y.shape[1]))
+
+    # ADDING POISSON NOISE TO SPECTROGRAM
+    for i in tqdm.tqdm(range(X.shape[0])):
+        x = X[i]
+        y = Y[i]
+
+        # POISSON NOISE ON SPECTRUM
+        x_poisson = np.random.poisson(x, size=(100, len(x)))
+        x_poisson[:, -3:] = x[-3:]
+
+        # TRANSFORM THE DATA
+        x_poisson_transform = data_loader.validation_gen.scaler.x_scaler.transform(x_poisson)
+
+        # MAKE A PREDICTION
+        y_hat = model.predict(x_poisson_transform)
+
+        # INVERSE TRANSFORM THE PREDICTIONS
+        y_hat_transform = data_loader.validation_gen.scaler.y_scaler.inverse_transform(y_hat)
+        
+        idx = i * 100
+        targets[idx:idx+100] = np.tile(y, (100, 1))
+        predictions[idx:idx+100] = y_hat_transform
+
+    ns.utils.store_predictions(
+        x=None,
+        y=targets,
+        predictions=predictions,
+        args=args,
+        data_partition='poisson')
