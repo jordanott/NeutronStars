7649bae0756e8a2941afc0dc8f371d1360936114
Jordan Ott <jott1@uci.edu>
2021-04-24 12:40:14 -0700

    Preprocessing for gz and dat

--- neutron_stars/data_loader/preprocessing.py
+++ neutron_stars/data_loader/preprocessing.py
@@ -27,44 +27,64 @@ from tqdm import tqdm
 from glob import iglob
 
 
-def parse_file(file_name):
+def extra_dat(f):
+    for i, line in enumerate(f):
+        pass
+
+    idx = 0
+    num_lines = i // 7
+    num_coefficients = 4 if '4Param' in file_name else 2
+    star_nums = np.zeros((num_lines, 1), dtype=np.int)
+    details = np.zeros((num_lines, 5), dtype=np.float)
+    coefficients = np.zeros((num_lines, num_coefficients), dtype=np.float)
+    spectra = np.zeros((num_lines, 1024), dtype=np.float)
+
+    f.seek(0)
+    f.readline()
+    while True:
+        star_num = f.readline()#.decode('UTF-8')
+        if not star_num: break
+        star_num = int(star_num.replace('# Star', ''))
+        _ = f.readline()
+
+        coefficient = f.readline()#.decode('UTF-8')
+        coefficient = list(map(float, coefficient.split('   ')))
+        _ = f.readline()
+
+        detail = f.readline()#.decode('UTF-8')
+        detail = list(map(float, detail.split('\t')))
+        _ = f.readline()
+
+        spectrum = f.readline()#.decode('UTF-8')
+        spectrum = list(map(float, spectrum.split(', ')[:-1]))
+
+        star_nums[idx] = star_num
+        details[idx] = detail
+        coefficients[idx] = coefficient[1:]
+        spectra[idx] = spectrum
+        idx += 1
+
+    return star_nums, details, coefficients, spectra
+
+
+def parse_file_dat(file_name):
+
+    with open(file_name, 'r') as f:
+        star_nums, details, coefficients, spectra = extra_dat(f)
+
+    np.savez(
+        file_name.replace('.dat', '.npz'),
+        star_nums=star_nums,
+        details=details,
+        coefficients=coefficients,
+        spectra=spectra,
+    )
+
+
+def parse_file_dat_gz(file_name):
 
     with gzip.open(file_name, 'r') as f:
-        for i, line in enumerate(f):
-            pass
-
-        idx = 0
-        num_lines = i // 7
-        num_coefficients = 4 if '4Param' in file_name else 2
-        star_nums = np.zeros((num_lines, 1), dtype=np.int)
-        details = np.zeros((num_lines, 5), dtype=np.float)
-        coefficients = np.zeros((num_lines, num_coefficients), dtype=np.float)
-        spectra = np.zeros((num_lines, 1024), dtype=np.int)
-
-        f.seek(0)
-        f.readline()
-        while True:
-            star_num = f.readline().decode('UTF-8')
-            if not star_num: break
-            star_num = int(star_num.replace('# Star', ''))
-            _ = f.readline()
-
-            coefficient = f.readline().decode('UTF-8')
-            coefficient = list(map(float, coefficient.split('   ')))
-            _ = f.readline()
-
-            detail = f.readline().decode('UTF-8')
-            detail = list(map(float, detail.split('\t')))
-            _ = f.readline()
-
-            spectrum = f.readline().decode('UTF-8')
-            spectrum = list(map(int, spectrum.split(', ')[:-1]))
-
-            star_nums[idx] = star_num
-            details[idx] = detail
-            coefficients[idx] = coefficient[1:]
-            spectra[idx] = spectrum
-            idx += 1
+        star_nums, details, coefficients, spectra = extra_dat(f)
 
     np.savez(
         file_name.replace('.dat.gz', '.npz'),
@@ -77,7 +97,10 @@ def parse_file(file_name):
 
 if __name__ == '__main__':
     # tar -xvzf /baldig/physicstest/NeutronStarsData/res.tgz
-    all_files = list(iglob(DATA_DIR + '*.dat.gz'))
+    # all_files = list(iglob(DATA_DIR + '*.dat.gz'))
+
+    # scp -r jott1@gplogin2.ps.uci.edu:/DFS-L/DATA/atlas/whiteson/ns/res_nonoise10x/ /baldig/physicstest/NeutronStarsData/
+    all_files = list(iglob('/baldig/physicstest/NeutronStarsData/res_nonoise10x/*.dat'))
 
     for file_name in tqdm(all_files):
-        parse_file(file_name)
+        parse_file_dat(file_name)
