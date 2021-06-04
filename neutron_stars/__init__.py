32f4496139169fba59ae40645521d3b3497267e8
Jordan Ott <jott1@uci.edu>
2021-06-03 17:35:52 -0700

    Package

--- neutron_stars/__init__.py
+++ neutron_stars/__init__.py
@@ -1,5 +1,5 @@
 from .args import parse_args
-from .predictor import Predictor
+from .predictor import SpectraGenerator
 from .data_loader import DataLoader
 from . import models, utils, analysis
 from .config import paradigm_settings, PARADIGMS, DATA_DIR, get_paradigm_opts
