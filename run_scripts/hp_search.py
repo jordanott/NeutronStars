12f840a59a3142180f9fb8e98c4f488a06e15610
Jordan Ott <jordanott365@gmail.com>
2021-06-12 14:00:28 -0400

    Hyperparameter search in run_scripts folder

--- run_scripts/hp_search.py
+++ run_scripts/hp_search.py
@@ -1,5 +1,16 @@
 """
-tf2 runner.py --paradigm spectra+star2eos --model_type transformer --max_concurrent 16 --name _v2
+# Changes to run from flight
+# Test zero mean scaler
+tf2 run_scripts/hp_search.py --paradigm spectra2eos --model_type transformer --max_concurrent 16 --name _0mean_v1
+
+# Test only training m_2
+tf2 run_scripts/hp_search.py --paradigm spectra2m_2 --model_type transformer --max_concurrent 16 --name _0mean_v1
+
+# Redo Spectra Generator with conv and relu output
+# Test edits in models/models.py
+tf2 main.py --paradigm mr+stars2spectra --conv_branch --scaler_type none+none2none
+# Then run
+tf2 run_scripts/hp_search.py --paradigm mr+star2spectra --max_concurrent 16 --name _v2
 """
 
 import os
@@ -20,25 +31,23 @@ args = parser.parse_args()
 
 os.makedirs(args.output_dir, exist_ok=True)
 
-parameters = [
-    sherpa.Choice('mass_threshold', [6]),
-    sherpa.Discrete('num_layers', [1, 12]),
-    sherpa.Choice('num_nodes', list(range(64, 2049, 64))),
-    sherpa.Choice('batch_norm', [0, 1]),
-    sherpa.Continuous('dropout', [0, 1]),
-    sherpa.Choice('skip_connections', [0, 1]),
-    sherpa.Continuous('lr', [0.0001, 0.005]),
-    sherpa.Continuous('lr_decay', [0.8, 1.]),
-    sherpa.Choice('activation', list(ns.models.AVAILABLE_ACTIVATIONS.keys())),
-    sherpa.Choice('scaler_type', ns.data_loader.scaler_combinations_for_paradigm(args.paradigm)),
-    sherpa.Choice('loss_function', ['mse', 'mean_absolute_percentage_error', 'huber']),
-]
+parameters = [sherpa.Choice('mass_threshold', [6]),
+              sherpa.Discrete('num_layers', [1, 12]),
+              sherpa.Choice('num_nodes', list(range(64, 2049, 64))),
+              sherpa.Choice('batch_norm', [0, 1]),
+              sherpa.Continuous('dropout', [0, 1]),
+              sherpa.Choice('skip_connections', [0, 1]),
+              sherpa.Continuous('lr', [0.0001, 0.005]),
+              sherpa.Continuous('lr_decay', [0.8, 1.]),
+              sherpa.Choice('activation', list(ns.models.AVAILABLE_ACTIVATIONS.keys())),
+              sherpa.Choice('scaler_type', ns.data_loader.scaler_combinations_for_paradigm(args.paradigm)),
+              sherpa.Choice('loss_function', ['mse', 'mean_absolute_percentage_error', 'huber'])]
 
 if args.model_type == 'transformer':
-    parameters.extend([sherpa.Choice('num_stars', [5, 10]),
+    parameters.extend([sherpa.Choice('num_stars', [15]),
                        sherpa.Choice('transformer_op', ['max', 'min', 'gather'])])
 
-elif 'spectra' in args.paradigm.split('2')[0]:
+elif 'spectra' in args.paradigm:
     parameters.append(
         sherpa.Choice('conv_branch', [0, 1])
     )
